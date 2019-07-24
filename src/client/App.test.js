23a9e5cd60210abbccaaef943fe6ccc51c36c912
ppichier <pierantonio.pichierri@gmail.com>
2019-07-24 20:19:48 +0200

    [ADD] config setupTests.js

--- src/client/App.test.js
+++ src/client/App.test.js
@@ -8,7 +8,7 @@ it("renders without crashing", () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-/* it("sums numbers", () => {
+it("sums numbers", () => {
   expect(1 + 2).toEqual(3);
   expect(2 + 2).toEqual(4);
-}); */
+});
